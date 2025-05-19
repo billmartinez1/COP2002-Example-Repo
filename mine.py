ab3ae216ce0c8c7d321f29ef5aa7d8ad8ed6f502
Bill Martinez <gam0811@yahoo.com>
2025-05-18 20:18:01 -0400

    Please enter the commit message for your changes. Lines starting

--- mine.py
+++ mine.py
@@ -1,10 +1,10 @@
 # Write a line of code that prints "Hello world!".
 print ("Hello World!")
 
-def hello_world("Hello world!"):  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
+def hello_world():  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   return hw
 
- hw = ("Hello world!")
+hw = ("Hello world!")
 
 
 #def hello_world():
