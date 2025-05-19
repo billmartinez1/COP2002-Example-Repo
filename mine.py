b43fa6389aa40cd33d62858b50809cffc437f6e7
Bill Martinez <gam0811@yahoo.com>
2025-05-18 20:29:42 -0400

    update

--- mine.py
+++ mine.py
@@ -7,7 +7,3 @@ def hello_world():  # Create a variable that has the value of "Hello world!" so 
 hw = ("Hello world!")
 
 
-#def hello_world():
-  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  
-  #return variable_name
