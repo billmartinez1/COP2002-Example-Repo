1fac2a771ec5ea353d325152e58bdbd37ed12937
Bill Martinez <gam0811@yahoo.com>
2025-05-23 09:02:57 -0400

    Update MyFile.py

--- MyFile.py
+++ MyFile.py
@@ -1,7 +1,7 @@
 # Creating a project for GitHub
 # By Bill Martinez
  
-print("COP2002 Fall 2030")
+print("COP2002 Fall 2025")
 
 print ()
 print("I'm understanding how to use GitHub to keep track of changes!")
